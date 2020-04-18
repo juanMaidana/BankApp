dc02868f77e8e27bc0e3d7a54b01b49ddce39410
juanMaidana <gio.maidana21@gmail.com>
2020-04-18 04:42:41 -0400

    Practice completed with deposit opration and transaction information

--- src/main/java/org/example/controller/operators/DepositOperator.java
+++ src/main/java/org/example/controller/operators/DepositOperator.java
@@ -1,4 +1,20 @@
 package org.example.controller.operators;
 
-public class DepositOperator {
+import org.example.controller.interfaces.IAccountOperator;
+import org.example.model.Transaction;
+import org.example.model.TransactionType;
+import org.example.model.interfaces.IDataSaver;
+
+public class DepositOperator implements IAccountOperator {
+    private IDataSaver dataSaver;
+
+    public DepositOperator(IDataSaver dataSaver) {
+        this.dataSaver = dataSaver;
+    }
+    @Override
+    public boolean execute(int id, double amount) {
+        Transaction deposit = new Transaction(TransactionType.DEPOSIT, amount, id);
+        dataSaver.addTransaction(deposit);
+        return true;
+    }
 }
